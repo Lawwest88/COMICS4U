610d8f666a9fc810f1d7dd820ef3f244d6214108
Lawwest88 <lawrence-west@hotmail.com>
2022-04-03 18:02:59 -0500

    adding listing model and initial migration

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Listing(models.Model):
+    # item descriptions
+    title = models.CharField(max_length=250)
+    number = models.IntegerField(null=True)
+    description = models.TextField()
+    
+    # price descriptions
+    price = models.FloatField()
+    price_fixed = models.BooleanField(default=False)
+
+    # posted information
+    posted = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
