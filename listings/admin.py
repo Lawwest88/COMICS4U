db32aa5e241f7be7e2867c610c70f03c13f931dd
Lawwest88 <lawrence-west@hotmail.com>
2022-04-03 19:33:16 -0500

    added listing to admin

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from listings.models import Listing
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('title','number','price','created')
+
+admin.site.register(Listing, ListingAdmin)
